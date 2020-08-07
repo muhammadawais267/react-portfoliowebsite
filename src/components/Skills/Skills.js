b84e0c30a95da5fdf96d7ffd5cf28440e13683e6
webdesigner295 <webdesigner295@gmail.com>
2020-08-07 17:12:12 +0500

    Skiilss

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-import { Container, Row, Col, Button, Image } from 'react-bootstrap';
+import { Container, Row, Col, Button} from 'react-bootstrap';
 import './Skills.css';
 
 function Skills() {
